2fc6af8ae2d690e588dc3c92e7281d5220c11f16
random-frontlines <leezinu1980@gmail.com>
2026-05-13 19:44:00 -0500

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,20 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from '../firebase-applet-config.json';
+
+// This is your new manual config for Neon Nexus Remastered
+const firebaseConfig = {
+  apiKey: "AiZaSyAkU09HI8X4Y69VNxaF4K6XZqe7bVJ1U",
+  authDomain: "neon-nexus-remastered.firebaseapp.com",
+  projectId: "neon-nexus-remastered",
+  storageBucket: "neon-nexus-remastered.firebasestorage.app",
+  messagingSenderId: "534985661126",
+  appId: "1:534985661126:web:91e7f404e4718d508715c7",
+  measurementId: "G-0X34RB2V1D"
+};
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+
+// We simplified this line to use your default database
+export const db = getFirestore(app);
 export const auth = getAuth(app);
